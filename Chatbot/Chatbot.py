d8c30c7f2cd8c205f929d8dcb6ccd0149e31caa1
Edurad Koniaiev <e.p.koniaiev@student.khai.edu>
2022-10-17 20:35:41 +0300

    chatbot 1st stage

---
+++ Chatbot/Chatbot.py
@@ -0,0 +1,33 @@
+bot_name = "Chappi"
+birth_year = "2022"
+print("Hello! My name is {0}.\nI was created in {1}".format(bot_name, birth_year))
+
+user_name = input("Please, remind me your name.\n")
+print("What a great name you have,", user_name)
+
+print("Let me guess your age.")
+remainder3 = int(input("Enter remainders of dividing your age by 3, 5 and 7.\n"))
+remainder5 =int (input(""))
+remainder7 = int(input(""))
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print("Your age is ",age ,";that's a good time to start programming!")
+
+number_of_user = int(input("Now I will prove to you that I can count to any number you want.\n"))
+for i in range (0, number_of_user+1):
+    print(i,"!")
+print("Completed, have a nice day!")
+
+print('''Let's test your programming knowledge.\nWhat is PyCharm?
+1. Just a code editor
+2. IDE
+3. Photoshop
+4. Potatoe :D''')
+Start_Stop = True
+while Start_Stop :
+    answer = input("")
+    if answer == "2" or answer == "IDE":
+        print("Completed, have a nice day!")
+        Start_Stop = False
+    else:
+        print("Please, try again.")
+print("Congratulations, have a nice day!")
